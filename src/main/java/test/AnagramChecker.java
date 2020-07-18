7512974fb71472a0d69eb79566f2f057a4f8910e
Ichsan Siswoputranto <milunes@gmail.com>
2020-07-18 20:08:01 +1000

    Update AnagramChecker to ignore LowerCase/UpperCase and Space
    + add .gitignore
    + AnagramChecker now works for Case variation and Space

--- src/main/java/test/AnagramChecker.java
+++ src/main/java/test/AnagramChecker.java
@@ -2,34 +2,47 @@ package test;
 
 public class AnagramChecker {
 
-  String word1;
-  String word2;
+  String unfilteredWord1;
+  String unfilteredWord2;
+
+  String filteredWord1;
+  String filteredWord2;
 
   public Boolean isAnagram() {
 
-    char[] word1Array = this.word1.toCharArray();
-    char[] word2Array = this.word2.toCharArray();
+    // normalize the words first, make it lowercase and remove spaces
+    this.stringSpaceAndMakeLowerCase();
 
+    char[] word1Array = this.filteredWord1.toCharArray();
+    char[] word2Array = this.filteredWord2.toCharArray();
 
     if (word1Array.length == word2Array.length) {
-      if ( iterateStringLookForCharMatch(word1, word2) ){
-        if (iterateStringLookForCharMatch(word2, word1)){
+      if (iterateStringLookForCharMatch(filteredWord1, filteredWord2)) {
+        if (iterateStringLookForCharMatch(filteredWord2, filteredWord1)) {
 
-          System.out.println(">>> end result! "+word1+" and " +word1+" are anagram to each other");
-            return true;
-        }else{
-          System.out.println(">>> end result! "+word1+" and " +word1+" are NOT anagram to each other");
+          System.out
+              .println(">>> end result! " + unfilteredWord1 + " and " + unfilteredWord2 + " are anagram to each other");
+          return true;
+        } else {
+          System.out.println(
+              ">>> end result! " + unfilteredWord1 + " and " + unfilteredWord2 + " are NOT anagram to each other");
           return false;
         }
-      }else{
-        System.out.println(">>> end result! "+word1+" and " +word1+" are NOT anagram to each other");
+      } else {
+        System.out.println(
+            ">>> end result! " + unfilteredWord1 + " and " + unfilteredWord2 + " are NOT anagram to each other");
         return false;
       }
-    }else {
+    } else {
       System.out.println(">>> words are of different length NOT an anagram");
       return false;
     }
 
+  }
+
+  public void stringSpaceAndMakeLowerCase() {
+    this.filteredWord1 = this.unfilteredWord1.toLowerCase().replaceAll(" ", "");
+    this.filteredWord2 = this.unfilteredWord2.toLowerCase().replaceAll(" ", "");
   }
 
   public Boolean iterateStringLookForCharMatch(String word1, String word2) {
@@ -40,23 +53,20 @@ public class AnagramChecker {
     // same letters
     System.out.println("::: anagram check " + word1 + " vs " + word2);
 
-      for (char c : word1Array) {
+    for (char c : word1Array) {
 
-        System.out.println("   does " + c + " exist in " + word1);
-        if (this.word2.indexOf(c) > -1) {
-          System.out.println("   >>> " + c + " exist in " + word1);
-        } else {
-          System.out.println("   >>> " + c + " doesn't exist in " + word1);
-          return false;
-        }
-
-
+      System.out.println("   does " + c + " exist in " + word1 + "?");
+      if (word2.indexOf(c) > -1) {
+        System.out.println("   >>> " + c + " exist in " + word2);
+      } else {
+        System.out.println("   >>> " + c + " doesn't exist in " + word2);
+        return false;
       }
-      System.out.println(" all letters from `" + word1 + "` exist in `" + word2 +"`");
 
-      return true;
     }
+    System.out.println(" all letters from `" + word1 + "` exist in `" + word2 + "`");
 
-
+    return true;
+  }
 
 }
--- src/test/java/test/AnagramCheckerTest.java
+++ src/test/java/test/AnagramCheckerTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -8,44 +9,67 @@ import org.junit.Test;
 public class AnagramCheckerTest {
 
   @Test
-  public void two_words_are_anagram_of_each_other(){
+  public void two_words_are_anagram_of_each_other() {
 
     AnagramChecker anagramChecker = new AnagramChecker();
 
-    anagramChecker.word1 = "dad";
-    anagramChecker.word2 = "add";
+    anagramChecker.unfilteredWord1 = "dad";
+    anagramChecker.unfilteredWord2 = "add";
 
     assertTrue("the isAnagram method should return true because the two words supplied are anagram of each other",
-     anagramChecker.isAnagram());
-
+        anagramChecker.isAnagram());
 
   }
 
   @Test
-  public void two_words_are_NOT_anagram_of_each_other(){
+  public void two_words_are_NOT_anagram_of_each_other() {
 
     AnagramChecker anagramChecker = new AnagramChecker();
 
-    anagramChecker.word1 = "bad";
-    anagramChecker.word2 = "add";
+    anagramChecker.unfilteredWord1 = "bad";
+    anagramChecker.unfilteredWord2 = "add";
 
     assertFalse("the isAnagram method should return false because the two words supplied are NOT anagram of each other",
-     anagramChecker.isAnagram());
-
+        anagramChecker.isAnagram());
 
   }
 
   @Test
-  public void two_words_are_anagram_of_each_other_2(){
+  public void two_words_are_anagram_of_each_other_2() {
 
     AnagramChecker anagramChecker = new AnagramChecker();
 
-    anagramChecker.word1 = "madonnaoftherocks";
-    anagramChecker.word2 = "sodarktheconofman";
+    anagramChecker.unfilteredWord1 = "madonnaoftherocks";
+    anagramChecker.unfilteredWord2 = "sodarktheconofman";
 
     assertTrue("the isAnagram method should return false because the two words supplied are NOT anagram of each other",
-     anagramChecker.isAnagram());
+        anagramChecker.isAnagram());
 
+  }
+
+
+  @Test
+  public void strip_spaces_and_make_letters_lowercase(){
+    AnagramChecker anagramChecker = new AnagramChecker();
+
+    anagramChecker.unfilteredWord1 = "Madonna of The Rocks";
+    anagramChecker.unfilteredWord2 = "So Dark The Con of Man";
+
+    anagramChecker.stringSpaceAndMakeLowerCase();
+
+    assertEquals("`Madonna of The Rocks` should become `madonnaoftherocks`", "madonnaoftherocks", anagramChecker.filteredWord1 );
+    assertEquals("`So Dark The Con of Man` should become `sodarktheconofman`", "sodarktheconofman", anagramChecker.filteredWord2 );
+  }
+
+  @Test
+  public void two_words_are_anagram_of_each_other_3_with_CaseVariation_and_space(){
+    AnagramChecker anagramChecker = new AnagramChecker();
+
+    anagramChecker.unfilteredWord1 = "Madonna of The Rocks";
+    anagramChecker.unfilteredWord2 = "So Dark The Con of Man";
+
+    assertTrue("the isAnagram method should return false because the two words supplied are NOT anagram of each other",
+        anagramChecker.isAnagram());
 
   }
 
